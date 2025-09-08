f6571fa1d07ddc47d83bea4d171944d0dde84d6b
Stéphane Bressani <stephane@bressani.dev>
2025-09-08 13:08:10 +0200

    ok

--- src/main.rs
+++ src/main.rs
@@ -13,31 +13,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut buf = Vec::new();
     let _ = img.read_to_end(&mut buf).unwrap();
 
-    let pic = Pic::new(&buf).size(320 * 9525, 240 * 9525);
-
-    /*
-    let table = Table::new(vec![
-        TableRow::new(vec![
-            TableCell::new()
-                .set_borders(
-                    Borders::new()
-                        .top(Border::new().size(4))
-                        .bottom(Border::new().size(4))
-                        .left(Border::new().size(4))
-                        .right(Border::new().size(4))
-                )
-                .shading(Shading::new().fill("FFFF00"))
-                .add_paragraph(
-                    Paragraph::new().add_run(
-                        Run::new()
-                            .add_text("Cellule 1")
-                            .color("FF0000")
-                            .fonts("Arial")
-                            .bold()
-                    )
-                )
-        ])
-    ]);*/
+    let pic = Pic::new(&buf.as_slice()).size(320 * 9525, 240 * 9525);
 
     let table = Table::new(vec![TableRow::new(vec![
         TableCell::new()
